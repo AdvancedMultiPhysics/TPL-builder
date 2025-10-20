a8db97e1dca44393143d8b87d7b13a84ce5108f6
Grace Lydia Longo <glongo@lanl.gov>
2025-10-20 17:09:14 -0600

    updating tpl-builder spack package to reflect chnages in amp MR #791

--- spack-repo/v1/packages/tpl-builder/package.py
+++ spack-repo/v1/packages/tpl-builder/package.py
@@ -17,6 +17,7 @@ class TplBuilder(CMakePackage, CudaPackage, ROCmPackage):
     license("UNKNOWN")
 
     version("master", branch="master")
+    version("2.1.3", tag="2.1.3", commit="2c4fe7b60685c8a25f1b2b60dc8062f9f5ed84c9")
     version("2.1.2", tag="2.1.2", commit="cdb270395e1512da2f18a34a7fa6b60f1bcb790d")
     version("2.1.0", tag="2.1.0", commit="f2018b32623ea4a2f61fd0e7f7087ecb9b955eb5")
 
@@ -33,6 +34,18 @@ class TplBuilder(CMakePackage, CudaPackage, ROCmPackage):
     variant("petsc", default=False, description="Build with support for petsc")
     variant("trilinos", default=False, description="Build with support for trilinos")
     variant("test_gpus", default=-1, values=int, description="Build with NUMBER_OF_GPUs setting, defaults to use the number of gpus available")
+    variant("unified-memory", default=True, description="Build hypre with unified memory")
+    variant(
+        "cxxstd",
+        default="17",
+        values=("17", "20", "23"),
+        multi=False,
+        description="C++ standard",
+    )
+
+    conflicts("cxxstd=20", when="@:2.1.2") #c++ 20 is only compatible with tpl-builder 2.1.3 and up
+    conflicts("cxxstd=23", when="@:2.1.2") #c++ 23 is only compatible with tpl-builder 2.1.3 and up
+    conflicts("+unified-memory", when="~rocm~cuda")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
@@ -56,13 +69,18 @@ class TplBuilder(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("hypre+mixedint", when="+hypre")
     depends_on("kokkos", when="+kokkos")
 
+    depends_on("kokkos+openmp", when="+kokkos+openmp")
     depends_on("kokkos+cuda+cuda_constexpr", when="+kokkos+cuda")
     depends_on("kokkos+rocm", when="+kokkos+rocm")
-    depends_on("hypre+cuda+unified-memory", when="+hypre+cuda")
-    depends_on("hypre+rocm+unified-memory", when="+hypre+rocm")
 
-    depends_on("hypre~shared", when="~shared+hypre")
-    depends_on("hypre+shared", when="+shared+hypre")
+
+    hypre_depends = ["shared", "cuda", "rocm", "openmp"]
+
+    for v in hypre_depends:
+        depends_on(f"hypre+{v}", when=f"+{v}+hypre")
+        depends_on(f"hypre~{v}", when=f"~{v}+hypre")
+    
+
     depends_on("blas", when="+lapack")
     depends_on("lapack", when="+lapack")
 
@@ -124,7 +142,9 @@ class TplBuilder(CMakePackage, CudaPackage, ROCmPackage):
             self.define('CMAKE_C_COMPILER',   spack_cc),
             self.define('CMAKE_CXX_COMPILER', spack_cxx),
             self.define('CMAKE_Fortran_COMPILER', spack_fc),
+            self.define_from_variant("CMAKE_CXX_STANDARD", "cxxstd")
         ]
+
 
         if spec.satisfies("+cuda"):
             cuda_arch = spec.variants["cuda_arch"].value
@@ -156,10 +176,13 @@ class TplBuilder(CMakePackage, CudaPackage, ROCmPackage):
                     ]
                 )
                 
-        if spec.satisfies("+mpi") and spec.satisfies("+rocm"):
+        if spec.satisfies("+mpi +rocm"):
             options.extend( [self.define('CMAKE_HIP_HOST_COMPILER', spec['mpi'].mpicxx),
                              self.define('CMAKE_HIP_FLAGS', spec['mpi'].headers.include_flags),
                              ] )
+
+        if spec.satisfies("+hypre~unified-memory"):
+            options.extend( [self.define('HYPRE_DISABLE_UNIFIED_MEMORY', True)] )
 
         tpl_list = []
 
